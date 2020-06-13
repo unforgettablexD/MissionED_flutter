d74bdba145053ce9b162747e97a6b17e68fe077b
ctrl-alt-beep <nishant.raj3009@gmail.com>
2020-06-13 19:50:37 +0530

    push

--- missioned_flutter_app/lib/main.dart
+++ missioned_flutter_app/lib/main.dart
@@ -7,35 +7,34 @@ void main() {
     debugShowCheckedModeBanner: false,
     home: new MyApp(),
     routes: <String, WidgetBuilder>{
-      "/intro2": (BuildContext context) =>new introduction2(),
-      "/intro3": (BuildContext context) =>new introduction3()
+      "/intro2": (BuildContext context) => new introduction2(),
+      "/intro3": (BuildContext context) => new introduction3()
     },
   ));
 }
 
 class MyApp extends StatefulWidget {
   @override
-  MyAppState createState() =>MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class MyAppState extends State<MyApp>{
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      seconds: 3,
+      seconds: 10,
       backgroundColor: Colors.white,
-      image: Image.asset('assets/output-onlinepngtools (88).png'),
+      image: Image.asset('assets/images/output-onlinepngtools (88).png'),
       photoSize: 150,
       navigateAfterSeconds: introduction1(),
-
     );
     // TODO: implement build
   }
 }
 
-class introduction1 extends StatelessWidget{
+class introduction1 extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("MissionED"),
@@ -45,49 +44,93 @@ class introduction1 extends StatelessWidget{
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-
               new Text("Introduction page 1"),
-              new RaisedButton(
-                  onPressed: () {Navigator.of(context).pushNamed("/intro2");}
-
-              )
+              new RaisedButton(onPressed: () {
+                Navigator.of(context).pushNamed("/intro2");
+              })
             ],
           ),
         ),
-
       ),
     );
   }
 }
 
-class introduction2 extends StatelessWidget{
+class introduction2 extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  _introduction2State createState() => _introduction2State();
+}
+
+class _introduction2State extends State<introduction2> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("MissionED"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+              height: 140,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: ExactAssetImage('assets/images/blank_profile_pic.png'),
+                fit: BoxFit.cover,
+              )),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: new Container(
         child: new Center(
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-
               new Text("Introduction page 2"),
-              new RaisedButton(onPressed: () {Navigator.of(context).pushNamed("/intro3");})
-
+              new RaisedButton(onPressed: () {
+                Navigator.of(context).pushNamed("/intro3");
+              })
             ],
           ),
         ),
-
       ),
     );
   }
 }
 
-class introduction3 extends StatelessWidget{
+class introduction3 extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("MissionED"),
@@ -97,16 +140,12 @@ class introduction3 extends StatelessWidget{
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-
               new Text("Introduction page 3"),
               new RaisedButton(onPressed: null)
-
             ],
           ),
         ),
-
       ),
     );
   }
 }
-
